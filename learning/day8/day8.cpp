8184fa4f632639e9dab0b6f1515dffa310c7ccfa
liangchengbo <466312597@qq.com>
2019-02-14 17:07:53 +0100

    a

--- learning/day8/day8.cpp
+++ learning/day8/day8.cpp
@@ -18,7 +18,8 @@
 class Animal
 {
 //private:
-	static int count;//static 指定本cpp才能使用，private表示类内才能使用
+	static int count;//static的话不论创建多少个对象，变量只有一个;但如果非static的话，就每个对象都有一个独立的变量，
+	//private表示类内才能使用
 
 	 //或者类外定义int Animal::count=0;初始化为0，而且是定义的时候初始化，而且只能在类外定义
 	//每个对象被创建的时候，都有一个this指针指向本对象
@@ -39,7 +40,7 @@ public:
 		{count--;
 		std::cout << Name<<"挂掉了";}
 	void eat()
-		{std::cout << "动物我在吃"<< std::endl;}
+		{std::cout << "父类在吃"<< std::endl;}
 	void sleep()
 		{std::cout << "动物我在睡觉"<<std::endl;}
 };
@@ -50,16 +51,21 @@ int Animal::count = 0;//静态变量只能在外面定义，且定义时候初始化
 class Pig :public Animal
 {
 public:
-	//std::string Name;
-	Pig(std::string theName):Animal(theName)//继承构造方法,pig将theName传给Animal的构造方法，然后在进入下面自己的构造方法,其实下面可以直接空的{}
+	std::string localName;
+	Pig(std::string theName):Animal(theName)//继承构造方法,pig将theName传给Animal的构造方法，像初始化，
+		//然后在进入下面自己的构造方法,其实下面可以直接空的{}
 	{
+		std::cout<<"调用一下父类的普通方法:"<<Animal::Name<<std::endl;
 		std::cout << "子类构造" << std::endl;
 		std::cout << Name<<std::endl;//这时候名字为abo，父类构造方法吸收的名字
 		//Name = "我的名字是猪";//这里又改了名字，最终Name是这里改的
 	}
-	void climb()
+	void eat()//这里定义了个eat，父类也有eat，对象用eat的话，首先检查子类eat，如果有就不用父类的了，相当于屏蔽了父类eat
+		//这里子类的eat覆盖掉父类的eat了
+		//但还能调用父类eat
 	{
-		std::cout << "猪我在爬树"<< std::endl;
+		Animal::eat();//父类eat
+		std::cout << "子类在吃"<< std::endl;
 	}
 };
 
@@ -73,6 +79,8 @@ int main()
 	//std::cout<<Animal::count<<std::endl;//当count放在public的时候，就可以类外访问。放在private就是能类内，这里访问不了
 	std::cout << std::endl<< "现在有多少只:" <<Pig::getCount()<<std::endl;//不能用animal::count，因为这里不是类内，不能用private成员，
 	//类内的public成员函数可以对private变量做任何操作
+	pig.eat();
+
 	std::cout << "程序即将完成，完成了就立马析构所有成员" << std::endl;
 
 	return 0;//这里程序完成之后开始析构毁掉成员，而且后面的成员首先毁掉
